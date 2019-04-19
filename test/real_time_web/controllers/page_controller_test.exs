03d89a70841d23fc82204701d0c69122953962e1
Thomas Dola <thomasdolar@gmail.com>
2019-04-19 07:08:03 +0000

    fixes page controller test

--- test/real_time_web/controllers/page_controller_test.exs
+++ test/real_time_web/controllers/page_controller_test.exs
@@ -1,9 +1,13 @@
 defmodule RealTimeWeb.PageControllerTest do
   use RealTimeWeb.ConnCase
 
+  setup do
+    RealTimeWeb.Endpoint.subscribe("PAGE")
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert_receive %Phoenix.Socket.Broadcast{event: "WELCOME", topic: "PAGE", payload: %{"data" => _}}
+    assert_receive %Phoenix.Socket.Broadcast{event: "WELCOME", topic: "PAGE", payload: %{data: _}}
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
